ec09b8b53b36288ceeefec61d31074a0de694a59
John Lester Perjis <79300602+tyler-creator-69@users.noreply.github.com>
2025-11-08 21:30:56 +0800

    Delete educational_flascards_python directory

--- educational_flascards_python/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from flashcard_app import FlashcardApp
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    flashcard_app = FlashcardApp()
-    flashcard_app.show()
-    sys.exit(app.exec())
